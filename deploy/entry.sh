e2a49667ddb78501ee682e4edf0405df124a206c
Ari Lerner <arilerner@mac.com>
2015-12-27 21:35:08 -0800

    Updated config to include python path to sys automatically

--- deploy/entry.sh
+++ deploy/entry.sh
@@ -6,15 +6,15 @@ USER_FULL_NAME="${USER_FULL_NAME:-Compute container user}"
 USER_DIR="/home/${USER_LOGIN}"
 PASSWORD=${PASSWORD:-itsginger}
 
-IPYTHON_DIR="${USER_DIR}/.ipython"
-CONF_FILE="${IPYTHON_DIR}/ipython_notebook_config.py"
+JDIR="${USER_DIR}/.jupyter"
+CONF_FILE="${JDIR}/jupyter_notebook_config.py"
 NOTEBOOK_DIR="${USER_DIR}/notebooks"
-PASSWORD_FILE=${IPYTHON_DIR}/.pass
+PASSWORD_FILE=${JDIR}/.pass
 
 ##### Working stuff
 
 mkdir -p -m 700 ${USER_DIR}
-mkdir -p -m 700 ${IPYTHON_DIR}/security
+mkdir -p -m 700 ${JDIR}/security
 mkdir -p -m 700 ${NOTEBOOK_DIR}
 
 echo "Creating user ${USER_LOGIN} (${USER_UID}:${USER_GID})..."
@@ -22,20 +22,21 @@ id -u $USER_LOGIN &>/dev/null || adduser --disabled-password \
         --home "${USER_DIR}" \
         --uid "${USER_UID}" \
         --gecos "${USER_FULL_NAME},,," "${USER_LOGIN}" >/dev/null
-adduser "${USER_LOGIN}" compute-users 
+adduser "${USER_LOGIN}" compute-users
 
 cd "${USER_DIR}"
 chown -R $USER_LOGIN:compute-users $USER_DIR
 
 ## Create the config
 # SSL cert
-# openssl req -new -newkey rsa:2048 -days 2652 -nodes -x509 -subj "//CN=ipython.ari.io" -keyout ${IPYTHON_DIR}/security/ssl_${USER_LOGIN}.pem -out ${IPYTHON_DIR}/security/ssl_${USER_LOGIN}.pem
+# openssl req -new -newkey rsa:2048 -days 2652 -nodes -x509 -subj "//CN=ipython.ari.io" -keyout ${JDIR}/security/ssl_${USER_LOGIN}.pem -out ${JDIR}/security/ssl_${USER_LOGIN}.pem
 
 echo $PASSWORD > $PASSWORD_FILE
 
 cat<<EOF | sudo tee ${CONF_FILE}
 import os
 os.environ['SHELL'] = '/bin/bash'
+os.environ['PYTHONPATH'] = '$PYTHONPATH:${NOTEBOOK_DIR}'
 
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
@@ -43,13 +44,28 @@ c.NotebookApp.port = 8888
 c.NotebookApp.port_retries = 249
 c.NotebookApp.enable_mathjax = True
 c.NotebookApp.open_browser = False
-# c.NotebookApp.certfile = u'${IPYTHON_DIR}/security/ssl_${USER_LOGIN}.pem'
-c.NotebookApp.ipython_dir = u'${IPYTHON_DIR}'
+# c.NotebookApp.certfile = u'${JDIR}/security/ssl_${USER_LOGIN}.pem'
+# c.NotebookApp.JDIR = u'${JDIR}'
 
 from IPython.lib import passwd
 with open('${PASSWORD_FILE}', 'r') as fp:
     p = fp.read().strip()
 c.NotebookApp.password = passwd(p)
+
+## Include the normal things
+c.InteractiveShellApp.exec_lines = [
+  '%matplotlib inline',
+  '%load_ext autoreload',
+  '%autoreload 2',
+]
+
+c.InteractiveShell.autoindent = True
+c.InteractiveShell.colors = 'LightBG'
+c.InteractiveShell.confirm_exit = False
+c.InteractiveShell.deep_reload = True
+c.InteractiveShell.editor = 'nano'
+c.InteractiveShell.xmode = 'Context'
+
 c.IPKernelApp.pylab = 'inline'
 c.InteractiveShellApp.matplotlib = 'inline'
 c.IPKernelApp.matplotlib = 'inline'
@@ -57,4 +73,4 @@ c.NotebookApp.notebook_dir = os.path.expanduser('~/notebooks/')
 EOF
 
 SUDO="sudo"
-IPYTHONDIR=${IPYTHON_DIR} HOME="${USER_DIR}" $SUDO -E -u "${USER_LOGIN}" ${CMD:-/bin/bash --login -c "jupyter notebook --config=${CONF_FILE}"}
+HOME="${USER_DIR}" $SUDO -E -u "${USER_LOGIN}" ${CMD:-/bin/bash --login -c "jupyter notebook --config=${CONF_FILE}"}
